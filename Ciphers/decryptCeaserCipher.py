c11053d5980b22486d925537ff3fe1e67d1b8593
thomassldn <003686838@coyote.csusb.edu>
2019-07-18 15:36:05 -0700

    Update decryptCeaserCipher.py

--- Ciphers/decryptCeaserCipher.py
+++ Ciphers/decryptCeaserCipher.py
@@ -30,3 +30,74 @@ for key in range(len(ALPHABET)):
    print('Key #%s: %s' % (key, decryptedMsg))
 
 
+   
+# Sample Output: 
+$ python decryptCeaserCipher.py
+#Enter a message to decrypt using the ceaser cipher: guv6Jv6Jz!J6rp5r7Jzr66ntrM
+#Key #0: guv6Jv6Jz!J6rp5r7Jzr66ntrM
+#Key #1: ftu5Iu5Iy I5qo4q6Iyq55msqL
+#Key #2: est4Ht4Hx0H4pn3p5Hxp44lrpK
+#Key #3: drs3Gs3Gw9G3om2o4Gwo33kqoJ
+#Key #4: cqr2Fr2Fv8F2nl1n3Fvn22jpnI
+#Key #5: bpq1Eq1Eu7E1mkzm2Eum11iomH
+#Key #6: aopzDpzDt6Dzljyl1DtlzzhnlG
+#Key #7: ZnoyCoyCs5CykixkzCskyygmkF
+#Key #8: YmnxBnxBr4BxjhwjyBrjxxfljE
+#Key #9: XlmwAmwAq3AwigvixAqiwwekiD
+#Key #10: Wklv.lv.p2.vhfuhw.phvvdjhC
+#Key #11: Vjku?ku?o1?ugetgv?oguucigB
+#Key #12: Uijt!jt!nz!tfdsfu!nfttbhfA
+#Key #13: This is my secret message.
+#Key #14: Sghr0hr0lx0rdbqds0ldrrZfd?
+#Key #15: Rfgq9gq9kw9qcapcr9kcqqYec!
+#Key #16: Qefp8fp8jv8pbZobq8jbppXdb
+#Key #17: Pdeo7eo7iu7oaYnap7iaooWca0
+#Key #18: Ocdn6dn6ht6nZXmZo6hZnnVbZ9
+#Key #19: Nbcm5cm5gs5mYWlYn5gYmmUaY8
+#Key #20: Mabl4bl4fr4lXVkXm4fXllTZX7
+#Key #21: LZak3ak3eq3kWUjWl3eWkkSYW6
+#Key #22: KYZj2Zj2dp2jVTiVk2dVjjRXV5
+#Key #23: JXYi1Yi1co1iUShUj1cUiiQWU4
+#Key #24: IWXhzXhzbnzhTRgTizbThhPVT3
+#Key #25: HVWgyWgyamygSQfShyaSggOUS2
+#Key #26: GUVfxVfxZlxfRPeRgxZRffNTR1
+#Key #27: FTUewUewYkweQOdQfwYQeeMSQz
+#Key #28: ESTdvTdvXjvdPNcPevXPddLRPy
+#Key #29: DRScuScuWiucOMbOduWOccKQOx
+#Key #30: CQRbtRbtVhtbNLaNctVNbbJPNw
+#Key #31: BPQasQasUgsaMKZMbsUMaaIOMv
+#Key #32: AOPZrPZrTfrZLJYLarTLZZHNLu
+#Key #33: .NOYqOYqSeqYKIXKZqSKYYGMKt
+#Key #34: ?MNXpNXpRdpXJHWJYpRJXXFLJs
+#Key #35: !LMWoMWoQcoWIGVIXoQIWWEKIr
+#Key #36:  KLVnLVnPbnVHFUHWnPHVVDJHq
+#Key #37: 0JKUmKUmOamUGETGVmOGUUCIGp
+#Key #38: 9IJTlJTlNZlTFDSFUlNFTTBHFo
+#Key #39: 8HISkISkMYkSECRETkMESSAGEn
+#Key #40: 7GHRjHRjLXjRDBQDSjLDRR.FDm
+#Key #41: 6FGQiGQiKWiQCAPCRiKCQQ?ECl
+#Key #42: 5EFPhFPhJVhPB.OBQhJBPP!DBk
+#Key #43: 4DEOgEOgIUgOA?NAPgIAOO CAj
+#Key #44: 3CDNfDNfHTfN.!M.OfH.NN0B.i
+#Key #45: 2BCMeCMeGSeM? L?NeG?MM9A?h
+#Key #46: 1ABLdBLdFRdL!0K!MdF!LL8.!g
+#Key #47: z.AKcAKcEQcK 9J LcE KK7? f
+#Key #48: y?.Jb.JbDPbJ08I0KbD0JJ6!0e
+#Key #49: x!?Ia?IaCOaI97H9JaC9II5 9d
+#Key #50: w !HZ!HZBNZH86G8IZB8HH408c
+#Key #51: v0 GY GYAMYG75F7HYA7GG397b
+#Key #52: u90FX0FX.LXF64E6GX.6FF286a
+#Key #53: t89EW9EW?KWE53D5FW?5EE175Z
+#Key #54: s78DV8DV!JVD42C4EV!4DDz64Y
+#Key #55: r67CU7CU IUC31B3DU 3CCy53X
+#Key #56: q56BT6BT0HTB2zA2CT02BBx42W
+#Key #57: p45AS5AS9GSA1y.1BS91AAw31V
+#Key #58: o34.R4.R8FR.zx?zAR8z..v2zU
+#Key #59: n23?Q3?Q7EQ?yw!y.Q7y??u1yT
+#Key #60: m12!P2!P6DP!xv x?P6x!!tzxS
+#Key #61: lz1 O1 O5CO wu0w!O5w  sywR
+#Key #62: kyz0Nz0N4BN0vt9v N4v00rxvQ
+#Key #63: jxy9My9M3AM9us8u0M3u99qwuP
+#Key #64: iwx8Lx8L2.L8tr7t9L2t88pvtO
+#Key #65: hvw7Kw7K1?K7sq6s8K1s77ousN
+
