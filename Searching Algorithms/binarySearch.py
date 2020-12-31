7289c199a948a35d421c25fdf81afaec918e7db5
thomassldn <003686838@coyote.csusb.edu>
2020-12-31 11:14:32 -0800

    Add files via upload

---
+++ Searching Algorithms/binarySearch.py
@@ -0,0 +1,34 @@
+def binarySearch(arr, l, r, x ):
+    #check if r >= l
+    if r >= l :
+      mid = l + (r - l) // 2
+      #check if mid == x
+      if arr[mid] == x:
+        return mid
+      #if arr[mid] is greater than x, search left side
+      elif arr[mid] > x:
+        return binarySearch(arr, l, mid - 1, x )
+
+      else:
+        return binarySearch(arr, mid + 1, r, x)
+    else:
+      return -1
+
+#Execution begins
+arr = [5,9,33,1,99,44,2,7]
+
+print("Array before sorting:", arr)
+
+x = 99
+
+arr.sort()
+
+print("Array after sorting: ", arr)
+
+#call binarySearch, search for number
+index = binarySearch(arr, 0, len(arr) - 1, x)
+
+if index == -1:
+  print("Value does not exist in array!")
+else:
+  print("Value exist at index: ", index)
