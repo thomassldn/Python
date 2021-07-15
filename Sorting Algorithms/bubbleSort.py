859898956c080e408d77f3cab3432b7af3aa2b8d
thomassldn <003686838@coyote.csusb.edu>
2021-07-15 15:57:55 -0700

    Add files via upload

---
+++ Sorting Algorithms/bubbleSort.py
@@ -0,0 +1,20 @@
+def bubbleSort(arr):
+    temp = 0;
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+
+#Execution Begins
+arr = [5,1,89,3,9,3,33,42,7,99,100,34,78,95]
+
+#Print array before sorting
+print("Array before sorting: \n", arr)
+
+#Sort the Array
+bubbleSort(arr)
+
+#Print array before sorting
+print("Array after sorting: \n", arr)
