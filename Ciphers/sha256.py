114c9bc9718fe268b57632a2e44f382a1ec66ace
thomassldn <003686838@coyote.csusb.edu>
2021-07-21 15:03:21 -0700

    Add files via upload

---
+++ Ciphers/sha256.py
@@ -0,0 +1,22 @@
+#Sha-256 hash algorthim
+#Thomas
+
+import hashlib
+
+def encode(message):
+
+    encoded = hashlib.sha256(message.encode())
+    
+    return encoded
+
+#Get users message
+message = input("Enter string: ")
+print ("Message: ", message)
+
+#Encode Users Message and print it
+encoded_message = encode(message)
+print("Encoded: ", encoded_message)
+
+#Print the equivalent hexadecimal value.
+print("The hexadecimal equivalent of SHA256 is : ")
+print(encoded_message.hexdigest())
